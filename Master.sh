0f9543b161d40efa86d9c3e0852c849678703cf6
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-01-21 15:07:30 +0530

    update

--- Master.sh
+++ Master.sh
@@ -3,9 +3,11 @@
 
 pd=`pwd`
 StartDate=`date +"%m%d%Y%H%M%S"`
+cd ../docker-jmeter
+./build.sh
 
-wget http://apachemirror.wuchna.com//jmeter/binaries/apache-jmeter-5.2.1.zip
-unzip apache-jmeter-5.2.1.zip
-JMETER_HOME=${pd}/apache-jmeter-5.2.1/bin
-echo `ls $JMETER_HOME`
+#wget http://apachemirror.wuchna.com//jmeter/binaries/apache-jmeter-5.2.1.zip
+#unzip apache-jmeter-5.2.1.zip
+#JMETER_HOME=${pd}/apache-jmeter-5.2.1/bin
+#echo `ls $JMETER_HOME`
 
