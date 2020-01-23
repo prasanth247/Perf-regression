ab967d92332dd0f457ed3040db61c247e09ef8ce
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-01-23 16:21:07 +0530

    disable all jobs

--- Master.sh
+++ Master.sh
@@ -5,10 +5,12 @@ pd=`pwd`
 StartDate=`date +"%m%d%Y%H%M%S"`
 #cd ../docker-jmeter
 #./build.sh
-ls /opt/apache-jmeter-5.1.1/bin
-echo "done"
-#wget http://apachemirror.wuchna.com//jmeter/binaries/apache-jmeter-5.2.1.zip
-#unzip apache-jmeter-5.2.1.zip
-#JMETER_HOME=${pd}/apache-jmeter-5.2.1/bin
-#echo `ls $JMETER_HOME`
-
+#ls /opt/apache-jmeter-5.1.1/bin
+cp jmeterscripts/*.jar jmeterscripts/randomwords.txt /opt/apache-jmeter-5.1.1/lib
+echo "copied required files for creating batches"
+ls /opt/apache-jmeter-5.1.1/lib grep "GenerateData.jar"
+cd jmeterscripts
+#---Disable all active jobs---
+jmeter -n -D javax.net.ssl.keyStore=cc-stage-superuser.p12 -D javax.net.ssl.keyStorePassword=superuser -D javax.net.ssl.keyStoreType=pkcs12
+-t Disablealljobs.jmx -l Disablealljobs_$StartDate.jtl
+echo "disabled all jobs"
