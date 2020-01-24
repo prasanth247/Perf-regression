8bf86ca817cb9761be9d8c5c1cdd886f9f51cd22
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-01-24 16:20:33 +0530

    check awk

--- Master.sh
+++ Master.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # For any queries, please contact reddy.adalam@smarsh.com
-
 pd=`pwd`
 StartDate=`date +"%m%d%Y%H%M%S"`
-#cd ../docker-jmeter
-#./build.sh
-#ls /opt/apache-jmeter-5.1.1/bin
-cp jmeterscripts/*.jar jmeterscripts/randomwords.txt /opt/apache-jmeter-5.1.1/lib
+
+#cp jmeterscripts/*.jar jmeterscripts/randomwords.txt /opt/apache-jmeter-5.1.1/lib
 echo "copied required files for creating batches"
-#ls /opt/apache-jmeter-5.1.1/lib grep "GenerateData.jar"
 cd jmeterscripts
-#echo `pwd`
-#which jmeter
 #---Disable all active jobs---
-jmeter -n -D javax.net.ssl.keyStore=cc-stage-superuser.p12 -D javax.net.ssl.keyStorePassword=superuser -D javax.net.ssl.keyStoreType=pkcs12 -t Disablealljobs.jmx -l Disablealljobs_$StartDate.jtl
+#jmeter -n -D javax.net.ssl.keyStore=cc-stage-superuser.p12 -D javax.net.ssl.keyStorePassword=superuser -D javax.net.ssl.keyStoreType=pkcs12 -t Disablealljobs.jmx -l Disablealljobs_$StartDate.jtl
 echo "disabled all jobs"
+
+#---Get the count of messages before the test
+#rm getdata1.txt
+awk -F , '{ print $4,$7,$8}' OFS=, jobdetails.txt | sed 's/\r//g' >getdata1.txt
+awk -F, '{print $3,$2,$1}' OFS=, getdata1.txt > checkcount.txt
+cat attfeeds.txt >>checkcount.txt
+cat checkcount.txt
