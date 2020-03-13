c2dca9c87fe3563b5b7a73d3733260b351e6e451
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-03-13 22:30:13 +0530

    check status

--- Master.sh
+++ Master.sh
@@ -7,9 +7,10 @@ StartDate=`date +"%m%d%Y%H%M%S"`
 echo "copied required files for creating batches"
 cd jmeterscripts
 #---Disable all active jobs---
-#jmeter -n -D javax.net.ssl.keyStore=cc-stage-superuser.p12 -D javax.net.ssl.keyStorePassword=superuser -D javax.net.ssl.keyStoreType=pkcs12 -t Disablealljobs.jmx -l Disablealljobs_$StartDate.jtl
+jmeter -n -D javax.net.ssl.keyStore=cc-stage-superuser.p12 -D javax.net.ssl.keyStorePassword=superuser -D javax.net.ssl.keyStoreType=pkcs12 -t Disablealljobs.jmx -l Disablealljobs_$StartDate.jtl
 echo "disabled all jobs"
 
+echo "`cat Disablealljobs_$StartDate.jtl`"
 #---Get the count of messages before the test
 #rm getdata1.txt
 awk -F , '{ print $4,$7,$8}' OFS=, jobdetails.txt | sed 's/\r//g' >getdata1.txt
