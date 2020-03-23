f877c198ca6b10463c50ddb41b36f40abd963693
prasanth247 <32734886+prasanth247@users.noreply.github.com>
2020-03-23 19:33:31 +0530

    Update Master.sh

--- Master.sh
+++ Master.sh
@@ -36,9 +36,11 @@ echo "sending ATT batches"
 #----Dynamically calculating the expected count based on loop count----#
 
 attbatches=`cat ATT_Automation.jmx | grep "LoopController.loops\">" | cut -d ">" -f2 | cut -d "<" -f1`
-
+echo $attbatches
 att=`cat attfeeds.txt | wc -l`
+echo $att
 batchcount=`echo $((attbatches*1000 / att))`
+echo $batchcount
 awk -v num="$batchcount" -F, '{$2=$2+num;print}'  OFS=, intialcount.txt | sed 's/\r//g' > finalcount.txt
 paste -d, finalcount.txt attfeeds.txt | awk -F, '{print $1,$2,$4,$5}' OFS=, > finalcount1.txt
 echo "`cat finalcount1.txt`"
